15b6f223b7393da3e6a53e126312ccde4c7d2c58
Anton Butovich <abutovich@gmail.com>
2013-12-19 12:54:08 +0200

    Cleanup

--- cookbooks/haproxy/recipes/default.rb
+++ cookbooks/haproxy/recipes/default.rb
@@ -12,8 +12,6 @@ service "haproxy" do
 end
 
 haproxy_version = "1.5-dev19-0ubuntu1~precise"
-case node[:platform]
-when "debian", "ubuntu"
   include_recipe "apt"
   apt_repository "haproxy" do
     uri "http://ppa.launchpad.net/nilya/haproxy-1.5/ubuntu"
@@ -23,71 +21,18 @@ when "debian", "ubuntu"
     key "A6D3315B"
   end
 
-  package "haproxy" do
-    version haproxy_version
-    action :install
-  end
+package "haproxy" do
+  version haproxy_version
+  action :install
+end
   
+case node[:platform]
+when "debian", "ubuntu"
   bash "enable haproxy" do
     code <<-EEND
       echo 'ENABLED=1' > /etc/default/haproxy
     EEND
     only_if { File.exists? "/etc/default/haproxy" }
-  end
-
-when "centos"
-  if node['platform_version'].to_f < 6.0
-    remote_file "/tmp/haproxy.rpm" do
-      source "http://silverdire.com/files/repo/el5/x86_64/haproxy-1.5-dev19.el5.x86_64.rpm"
-      action :create
-    end
-    rpm_package "haproxy.rpm" do
-      source "/tmp/haproxy.rpm"
-      action :install    
-    end
-  end
-  if node['platform_version'].to_f > 6.0
-    remote_file "/tmp/haproxy-1.5-dev19.el6.x86_64.rpm" do
-      source "http://silverdire.com/files/repo/el6/x86_64/haproxy-1.5-dev19.el6.x86_64.rpm"
-      action :create_if_missing
-    end
-    rpm_package "haproxy-1.5-dev.rpm" do
-      source "/tmp/haproxy-1.5-dev19.el6.x86_64.rpm"
-      action :install
-    end
-  end
-end
-
-stats_cert="/etc/haproxy/stats.pem"
-if ( !node['haproxy']['stats_cert'].nil? )
-  if "#{node['haproxy']['stats_cert']}".match("^(http|ftp)")
-    #remote_file "#{Chef::Config[:file_cache_path]}/stats.pem" do
-    remote_file stats_cert do
-      source "#{node['haproxy']['stats_cert']}"
-    end
-  else
-    # File content
-    file stats_cert do
-      content node['haproxy']['stats_cert']
-      mode 00644
-      owner "root"
-      group "root"
-      action :create
-    end
-  end
-else
-  if ( ! node['cloud'].nil? ) &&( ! node['cloud']['public_hostname'].nil? )
-    hostname = node.cloud.public_hostname
-  else
-    hostname = node.fqdn
-  end
-  bash "generate #{stats_cert}" do
-    not_if { File.exists? stats_cert }
-    code <<-EEND
-      openssl genrsa -out #{stats_cert}.key 2048
-      openssl req -new -x509 -extensions v3_ca -days 1100 -subj "/CN=#{hostname}" -nodes -out #{stats_cert} -key #{stats_cert}.key
-      cat #{stats_cert}.key >> #{stats_cert}
-    EEND
   end
 end
 
@@ -98,7 +43,6 @@ template "/etc/haproxy/global.cfg" do
     :stats_pass => node['haproxy']['stats_pass'],
     :stats_uri => node['haproxy']['stats_uri'],
     :stats_port => node['haproxy']['stats_port'],
-    :stats_cert => stats_cert,
   })
 end
 
@@ -118,13 +62,6 @@ end
 
 cookbook_file "/usr/local/bin/delServer.sh" do
   source "delServer.sh"
-  owner "root"
-  group "root"
-  mode 00744
-end
-
-cookbook_file "/usr/local/bin/delServers.sh" do
-  source "delServers.sh"
   owner "root"
   group "root"
   mode 00744
